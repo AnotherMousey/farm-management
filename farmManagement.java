1c809b78161d43858e549f8a8a0a2bb99654ba95
mousey <hoangthanhbinh2006@gmail.com>
2024-10-07 17:21:02 +0700

    probably a better version of farmManagement

--- farmManagement.java
+++ farmManagement.java
@@ -1,7 +1,29 @@
+import java.util.ArrayList;
+
 public class farmManagement {
     public static void getAnimal() {
-        System.out.println("List of animals that can swim: pig, duck and fish");
-        System.out.println("List of animals that can walk: pig and duck");
+        pig p = new pig();
+        duck d = new duck();
+        fish f = new fish();
+        ArrayList<animal> animals = new ArrayList<>();
+
+        animals.add(p);
+        animals.add(d);
+        animals.add(f);
+
+        for (animal a : animals) {
+            System.out.println(a.getName());
+            if(a.isSwim()) {
+                System.out.println("Can swim");
+            } else {
+                System.out.println("Cannot swim");
+            }
+            if(a.isWalk()) {
+                System.out.println("Can walk");
+            } else {
+                System.out.println("Cannot walk");
+            }
+        }
     }
 
     public static void main(String[] args) {
